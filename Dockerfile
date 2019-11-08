4d3d6c5172162815645ec6972ea2be76491225fb
kmanuel <kruisz88@gmail.com>
2019-11-08 23:11:33 +0100

    add deploy step

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM node
 
+RUN apt-get install openssh-client
+
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod a+x /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
+echo '=================== Create deploy key to push ==================='
+mkdir /root/.ssh
+ssh-keyscan -t rsa github.com > /root/.ssh/known_hosts && \
+echo "${GIT_DEPLOY_KEY}" > /root/.ssh/id_rsa && \
+chmod 400 /root/.ssh/id_rsa
+echo '=================== Install node_modules ==================='
 npm install
+echo '=================== Build Gatsby Site ==================='
 npm run build
+echo '=================== Deploy Gatsby Site ==================='
+npm run deploy
