dc5b18a478c62cd35e31d1794e0c2339e531b02d
kmanuel <kruisz88@gmail.com>
2019-11-08 23:32:15 +0100

    add echo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+
+echo '=================== Set git username and email ===================' 
 git config --global user.name "Auto Deploy Action"
 git config --global user.email "autodeploy@action.com"
-
-echo '=================== Create deploy key to push ==================='
+echo '=================== Create deploy key to push ===================' 
 mkdir /root/.ssh
 ssh-keyscan -t rsa github.com > /root/.ssh/known_hosts && \
 echo "${GIT_DEPLOY_KEY}" > /root/.ssh/id_rsa && \
